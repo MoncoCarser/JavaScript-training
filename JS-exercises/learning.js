13e44318a3fdae5c47c444b57524c8251f3245b8
MoncoCarser <pjpakkala@gmail.com>
2023-02-16 21:12:03 +0100

    First exercises done

---
+++ JS-exercises/learning.js
@@ -0,0 +1,8 @@
+personsAge = prompt("Your age: ");
+Number(personsAge)
+if (personsAge >= 18) {
+	alert("Starting the shuttle.")
+}
+else if (personsAge < 18) {
+	alert("No. Turning off all controls and hiding the keys.")
+}

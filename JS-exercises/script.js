672a4b095a88d7335bdbbac1423e8700637916a4
MoncoCarser <pjpakkala@gmail.com>
2023-02-22 15:32:36 +0100

    Learned about JS loops.

--- JS-exercises/script.js
+++ JS-exercises/script.js
@@ -1,1 +1,29 @@
+var todos = [
+	"clean room",
+	"brush teeth",
+	"exercise",
+	"study Javascript",
+	"eat healthy",
+	];
 
+// var todosLength = todos.length;
+// for (var i=0; i < todosLength; i++) {
+// 	todos.pop();
+// }
+
+// forEach on uudempi tapa kirjoittaa for-looppi 
+todos.forEach(function(todo, i) {
+	console.log(todo + "!");
+})
+
+// var counterOne = 10;
+// while (counterOne > 0) {
+// 	console.log(counterOne);
+// 	counterOne--;
+// }
+
+// var counterTwo = 10;
+// do {
+// 	console.log(counterTwo);
+// 	counterTwo--;
+// } while (counterTwo > -4);

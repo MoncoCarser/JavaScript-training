5a6273df680541258681425b35cf155dcd03b923
MoncoCarser <pjpakkala@gmail.com>
2023-02-24 23:13:30 +0100

    Working to solve exercise regarding EventListener

--- DOM-exercises/script.js
+++ DOM-exercises/script.js
@@ -10,6 +10,8 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+var listItem = document.querySelector("ul");
+
 function inputLength() {
 	return input.value.length;
 }
@@ -33,6 +35,19 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function itemIsDone() {
+	console.log("Click registered")
+	return '<div class="done">'+ this +'</div>';
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+listItem.addEventListener("click", itemIsDone);
+
+// 1. If you click on the list item, it toggles the .done  class on and off.
+
+// 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
+
+// 3. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)

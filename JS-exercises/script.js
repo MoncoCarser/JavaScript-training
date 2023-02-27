5be6394b89ef061e7cff8d7e4210f474fd55197b
MoncoCarser <pjpakkala@gmail.com>
2023-02-27 21:37:40 +0100

    Function exercising

--- JS-exercises/script.js
+++ JS-exercises/script.js
@@ -1,58 +1,11 @@
-var database = [
-{
-	username: "andrei",
-	password: "supersecret"
-},
-{
-	username: "johnny11",
-	password: "alpha"
-},
-{
-	username: "sally",
-	password: "777"
-}
-	];
 
-var newsFeed = [
-{
-	username: "Bobby",
-	timeline: "So tired from learning so much today."
-},
-{
-	username: "Michael22",
-	timeline: "Best programming language is..."
-},
-{
-	username: "DudeMighty",
-	timeline: "I cracked it!"
-},
-{
-	username: "Sally",
-	timeline: "JS is so darn coooooooool!"
-}
-	];
-
-
-function isUserValid(username, password) {
-	for (var i=0; i < database.length; i++) {
-		if(database[i].username === username &&
-			database[i].password === password) {
-			return true;
-		}
-	}
-	return false;
+function wordLength(word) {
+	let length = word.length;
+	console.log("Word length is: " + length);
 }
 
-function signIn(username, password) {
-	if (isUserValid(username, password)) {
-		console.log(newsFeed);
-	} else {
-		alert("Sorry, username or password was not of acceptable quality.");
-	}
+do {
+let word = prompt("Give a word: ")
+wordLength(word);
 }
-
-
-var usernamePrompt = prompt("What is your username:");
-var passwordPrompt = prompt("What is your password:");
-
-signIn(usernamePrompt, passwordPrompt);
+while (prompt("Again?") === "yes");

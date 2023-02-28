2ec22033bf2ad83e3b21ec59fc16104be79f35d8
MoncoCarser <pjpakkala@gmail.com>
2023-02-28 16:51:40 +0100

    Added function exercises

--- JS-exercises/script.js
+++ JS-exercises/script.js
@@ -1,13 +1,27 @@
 
+function continueAfter1Second() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function multiWord(length) {
+	console.log(length**2);
+  const result = await continueAfter1Second();
+}
+
+
 function wordLength(word) {
 	let length = word.length;
 	console.log("Word length is: " + length);
+	multiWord(length)
 }
+
 
 do {
 let word = prompt("Give a word: ")
 wordLength(word);
-}
-while (prompt("Again?") === "yes");
-
-// add still a time delay, more functions, and more STUFF
+} while 
+(prompt("Again?") === "yes");
